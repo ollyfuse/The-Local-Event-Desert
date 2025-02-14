f3d19d435c4cf64c6793a91196b5f2f4551e4108
ollyfuse <cyotero26@gmail.com>
2025-02-14 20:57:07 +0200

    Modified style and added some popup box for details on first card in categories

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+
+const popup = document.getElementById('popup');
+const overlay = document.getElementById('overlay');
+const popupTitle = document.getElementById('popup-title');
+const popupDetails = document.getElementById('popup-details');
+
 const buttons = document.querySelectorAll('.category-btn');
 const allContents = document.querySelectorAll('.category-content');
 
@@ -13,5 +19,28 @@ buttons.forEach(button => {
         // showing the selected content
         const contentToShow = document.getElementById(contentIdd);
         contentToShow.classList.remove('hidden');
+    })
+
+    // for popup
+    document.querySelectorAll('.details-btn').forEach(btn => {
+        btn.addEventListener('click', () => {
+            const name = btn.dataset.name;
+            const date = btn.dataset.date;
+            const venue = btn.dataset.venue;
+            const cost = btn.dataset.venue;
+
+            popupTitle.textContent = name;
+            popupDetails.innerHTML = 'Date: ${date()}<br>Venue: ${venue}<br> Cost: ${cost}';
+
+            // // popup.textContent = `Details for ${btn.dataset.detail}`;
+            // popupContent.innerHTML = `<strong>${btn.dataset.detail}</strong>: Detailed event information goes here.`;
+            popup.classList.remove('hidden');
+            overlay.classList.remove('hidden');
+        })
+    })
+    // for popup overlay
+    overlay.addEventListener('click', () => {
+        popup.classList.add('hidden');
+        overlay.classList.add('hidden');
     })
 })
