d6d500b2011db2fd763fc86e8ad3ed5931c0dad0
ollyfuse <cyotero26@gmail.com>
2025-02-16 22:01:09 +0200

    Modifier the popup card on categories and added hover buttons on event button

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 const popup = document.getElementById('popup');
 const overlay = document.getElementById('overlay');
-const popupTitle = document.getElementById('popup-title');
-const popupDetails = document.getElementById('popup-details');
+// const popupTitle = document.getElementById('popup-title');
+// const popupDetails = document.getElementById('popup-details');
+const popupContent = document.getElementById('popup-content');
 
 const buttons = document.querySelectorAll('.category-btn');
 const allContents = document.querySelectorAll('.category-content');
@@ -12,10 +13,8 @@ buttons.forEach(button => {
     button.addEventListener('click', function() {
         // Hidding all category content
         allContents.forEach(content => content.classList.add('hidden'));
-
         // Getting data content form button clicked
         const contentIdd = button.getAttribute('data-content');
-
         // showing the selected content
         const contentToShow = document.getElementById(contentIdd);
         contentToShow.classList.remove('hidden');
@@ -24,16 +23,16 @@ buttons.forEach(button => {
     // for popup
     document.querySelectorAll('.details-btn').forEach(btn => {
         btn.addEventListener('click', () => {
-            const name = btn.dataset.name;
-            const date = btn.dataset.date;
-            const venue = btn.dataset.venue;
-            const cost = btn.dataset.venue;
+            // const name = btn.dataset.name;
+            // const date = btn.dataset.date;
+            // const venue = btn.dataset.venue;
+            // const cost = btn.dataset.venue;
 
-            popupTitle.textContent = name;
-            popupDetails.innerHTML = 'Date: ${date()}<br>Venue: ${venue}<br> Cost: ${cost}';
+            // popupTitle.textContent = name;
+            // popupDetails.innerHTML = 'Name: ${name} Date: ${date()}<br>Venue: ${venue}<br> Cost: ${cost}';
 
-            // // popup.textContent = `Details for ${btn.dataset.detail}`;
-            // popupContent.innerHTML = `<strong>${btn.dataset.detail}</strong>: Detailed event information goes here.`;
+            popup.textContent = `Details for ${btn.dataset.detail}`;
+            popupContent.innerHTML = `<strong>${btn.dataset.detail}</strong>: Detailed event information goes here.`;
             popup.classList.remove('hidden');
             overlay.classList.remove('hidden');
         })
